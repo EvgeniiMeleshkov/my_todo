ccfdaab7277cfcca38da5ad4771547bb6ed7a1bc
Evgenii <emeleshkov@mail.ru>
2022-08-27 00:08:34 +0300

    Types migration

--- src/api/tasks-api.ts
+++ src/api/tasks-api.ts
@@ -70,6 +70,15 @@ export const task = {
     deadline: null
 }
 
+export type UpdateTaskModelType = {
+    title?: string
+    description?: string
+    status?: TaskStatuses
+    priority?: TaskPriorities
+    startDate?: string
+    deadline?: string
+}
+
 
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
